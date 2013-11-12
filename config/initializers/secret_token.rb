654dba3e1a27ad2b63b57fab4e712edc89b796a5
bonet <bonetsu@gmail.com>
2013-11-12 22:18:19 +0700

    added environment variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Silverstar::Application.config.secret_token = '92dd8ab30b945b137475719dad4af96f1e9e59853721537a1716d376bcd31bbfc5682bec46b7d17ce3a55f3a9027c3669000ef589e6f224faae05939676efcf9'
+Silverstar::Application.config.secret_token = ENV['SECRET_TOKEN']

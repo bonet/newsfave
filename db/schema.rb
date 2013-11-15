a5941891149479c7d72ab6335ec111eebc146f5c
bonet <bonetsu@gmail.com>
2013-11-15 12:41:49 +0700

    removed null false requirement for username and email columns in database

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20131114125743) do
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
-    t.string   "username",             :null => false
-    t.string   "email",                :null => false
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
     t.string   "encrypted_password"

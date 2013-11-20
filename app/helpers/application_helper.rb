06c3a9715d82e34e79f720c8f693b3623995ecba
bonet <bonetsu@gmail.com>
2013-11-21 01:36:14 +0700

    removed unused helper functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,6 @@ module ApplicationHelper
     image_tag("logo-1.png", :alt => 'NewsFave Logo', :class => "")
   end
   
-  def valid_json? json_  
-    JSON.parse(json_)  
-    true  
-  rescue JSON::ParserError  
-    false  
-  end 
-  
   def retrieve_default_categories_per_publisher
     if signed_in? == false
       result = Curl.get(Rails.configuration.api_location_get_default_categories_per_publisher)
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,4 @@ module SessionsHelper
     (session.include? :user_id) && (session[:user_id].present?)
   end
   
-  def deny_access
-    redirect_to new_user_session_path, :notice => "Please Sign In to access this page"
-  end
-  
-  def deny_access_no_html
-    return render :nothing => true 
-  end
-  
 end
